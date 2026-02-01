7c185e6616bc3d177776bc6ceb00c57314783aa1
ShadowAdi <shadowshukla76@gmail.com>
2026-02-01 11:50:59 +0530

    fix dotenv issue

--- src/config/dotenv.ts
+++ src/config/dotenv.ts
@@ -5,5 +5,4 @@ configDotenv()
 export const PORT = process.env.PORT
 export const DB_URL = process.env.DB_URL
 export const CLIENT_URL = process.env.CLIENT_URL
-export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY
 export const NODE_ENV = process.env.NODE_ENV
